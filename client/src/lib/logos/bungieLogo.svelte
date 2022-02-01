6580a28673d7797a2ac81af7cf72a06615025a90
Ryan Boddy <rboddy@issgs.net>
2022-02-01 15:48:43 -0500

    TODO: Tweak Bungie Logo color

---
+++ client/src/lib/logos/bungieLogo.svelte
@@ -0,0 +1,15 @@
+<script>
+    const src = '/bungieLogo.png'
+</script>
+
+<img {src} alt='Bungie Logo' />
+
+<style>
+    img {
+        position: absolute;
+        bottom: 5%;
+        right: 100px;
+        height: 75px;
+        width: auto;
+    }
+</style>
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -2,8 +2,10 @@
     import Backgroundvid from '$lib/layout/backgroundvid.svelte';
     import MainMenu from '$lib/menu/mainMenu.svelte'
     import HaloLogo from '$lib/logos/haloLogo.svelte'
+    import BungieLogo from '$lib/logos/bungieLogo.svelte'
 </script>
 
 <Backgroundvid />
 <MainMenu />
 <HaloLogo />
+<BungieLogo />
