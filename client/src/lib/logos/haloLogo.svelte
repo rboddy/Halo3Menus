82d3a465e169f6f603acbe9e67c006dd915ec671
Ryan Boddy <rboddy@issgs.net>
2022-02-01 09:24:57 -0500

    Getting closer but having to mix fonts isn't ideal

---
+++ client/src/lib/logos/haloLogo.svelte
@@ -0,0 +1,26 @@
+<h1>HALO <span>3</span></h1>
+
+<style>
+    @font-face {
+        font-family: Halo3;
+        src: url(/fonts/Halo.ttf);
+    }
+    @font-face {
+        font-family: HaloReach;
+        src: url(/fonts/HaloReach.ttf);
+    }
+    h1 {
+        font-family: Halo3;
+        color: white;
+        font-size: 7em;
+        margin: 0;
+        font-weight: normal;
+        position: absolute;
+        bottom: 0;
+        right: 0;
+    }
+    span {
+        font-family: HaloReach;
+        font-size: 1.1em;
+    }
+</style>
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,7 +1,9 @@
 <script>
     import Backgroundvid from '$lib/layout/backgroundvid.svelte';
     import MainMenu from '$lib/menu/mainMenu.svelte'
+    import HaloLogo from '$lib/logos/haloLogo.svelte'
 </script>
 
 <Backgroundvid />
 <MainMenu />
+<HaloLogo />
