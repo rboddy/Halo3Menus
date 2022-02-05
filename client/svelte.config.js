958413266495bdf682fa8696801b2f77d6096652
rboddy <avidryanb@gmail.com>
2022-02-05 06:56:44 -0500

    Forgot to switch to prod adapter

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,30 +1,30 @@
-// /** @type {import('@sveltejs/kit').Config} */
-
-// import node from '@sveltejs/adapter-node';
-
-// const config = {
-// 	kit: {
-// 		adapter: node(),
-		
-// 		// Override http methods in the Todo forms
-// 		methodOverride: {
-// 			allowed: ['PATCH', 'DELETE']
-// 		}
-// 	}
-// };
-
-// export default config;
-
-import adapter from '@sveltejs/adapter-auto';
-
 /** @type {import('@sveltejs/kit').Config} */
+
+import node from '@sveltejs/adapter-node';
+
 const config = {
 	kit: {
-		adapter: adapter(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: node(),
+		
+		// Override http methods in the Todo forms
+		methodOverride: {
+			allowed: ['PATCH', 'DELETE']
+		}
 	}
 };
 
 export default config;
+
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter(),
+
+// 		// hydrate the <div id="svelte"> element in src/app.html
+// 		target: '#svelte'
+// 	}
+// };
+
+// export default config;
