e9027d8b47113774466f17be51d60d47313b0ec2
rboddy <avidryanb@gmail.com>
2022-02-05 05:47:42 -0500

    Did not realize the volume was atomic

--- client/src/lib/layout/backgroundvid.svelte
+++ client/src/lib/layout/backgroundvid.svelte
@@ -1,48 +1,49 @@
 <script>
-    import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 
-    const video = '/FlyBy.mp4'
+	const video = '/FlyBy.mp4';
 
-    var muted = true;
+	var muted = true;
 
-    function toggleMute() {
-        const videoElement = document.getElementById("myVideo");
-        videoElement.muted = !videoElement.muted;
-        muted = !muted;
-    }
+	function toggleMute() {
+		const videoElement = document.getElementById('myVideo');
+		videoElement.volume = 0.2;
+		videoElement.muted = !videoElement.muted;
+		muted = !muted;
+	}
 </script>
 
 <video playsinline autoplay muted loop id="myVideo">
-    <source src={video} type="video/mp4">
+	<source src={video} type="video/mp4" />
 </video>
-<button on:click|preventDefault = {toggleMute}>
-    {#if muted}
-    <i class="fa-solid fa-volume-xmark"></i>
-    {:else}
-    <i class="fa-solid fa-volume-high"></i>
-    {/if}
+<button on:click|preventDefault={toggleMute}>
+	{#if muted}
+		<i class="fa-solid fa-volume-xmark" />
+	{:else}
+		<i class="fa-solid fa-volume-high" />
+	{/if}
 </button>
 
 <style>
-    video {
-        object-fit: cover;
-        width: 100vw;
-        height: 100vh;
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: -1;
-    }
-    button {
-        background: none;
-        border: none;
-        color: white;
-        font-size: 20px;
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
-    button:hover {
-        cursor: pointer;
-    }
+	video {
+		object-fit: cover;
+		width: 100vw;
+		height: 100vh;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: -1;
+	}
+	button {
+		background: none;
+		border: none;
+		color: white;
+		font-size: 20px;
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+	button:hover {
+		cursor: pointer;
+	}
 </style>
