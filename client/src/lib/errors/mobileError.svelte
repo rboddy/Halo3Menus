4707c2e1a461d1283ea78bb23bf7f495198c9493
rboddy <avidryanb@gmail.com>
2022-02-05 06:53:48 -0500

    Adding in a mobile detection to present an error

---
+++ client/src/lib/errors/mobileError.svelte
@@ -0,0 +1,33 @@
+<div>
+	<h1>Uh Oh!</h1>
+
+	<p>
+		It looks like you're on a mobile device. This project is a recreation of the Halo 3 menu for the
+		web. While it's a beautiful menu, it doesn't work well on mobile devices. I'm going to work to
+		change this in the future, but for now, try this out on desktop when you get a chance! Thanks
+		for visiting!
+	</p>
+</div>
+
+<style>
+	@font-face {
+		font-family: Halo3;
+		src: url(/fonts/Halo.ttf);
+	}
+	div {
+		width: 100%;
+		height: 100vh;
+		background: -webkit-linear-gradient(#ffffff, #9bcce4);
+	}
+	h1 {
+		font-family: Halo3;
+		font-size: 3em;
+		text-align: center;
+		text-transform: uppercase;
+	}
+	p {
+		font-size: 1.5em;
+		font-family: Verdana, Geneva, Tahoma, sans-serif;
+		padding: 1em;
+	}
+</style>
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -3,9 +3,23 @@
 	import MainMenu from '$lib/menu/mainMenu.svelte';
 	import HaloLogo from '$lib/logos/haloLogo.svelte';
 	import BungieLogo from '$lib/logos/bungieLogo.svelte';
+	import MobileError from '$lib/errors/mobileError.svelte';
+
+	//mobile stuff
+	import mobile from 'is-mobile';
 </script>
 
-<Backgroundvid video="/FlyBy.mp4" />
-<MainMenu />
-<HaloLogo />
-<BungieLogo />
+{#if !mobile()}
+	<Backgroundvid video="/FlyBy.mp4" />
+	<MainMenu />
+	<HaloLogo />
+	<BungieLogo />
+{:else}
+	<MobileError />
+{/if}
+
+<style>
+	:global(body) {
+		margin: 0;
+	}
+</style>
